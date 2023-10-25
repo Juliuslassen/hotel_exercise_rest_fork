e26307bccd65fb212d72200911017c9c5c80ceff
Julius Lassen <julius5517@gmail.com>
2023-10-25 12:02:19 +0200

    hjkl

--- src/main/java/dk/lyngby/Main.java
+++ src/main/java/dk/lyngby/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        int id =0;
         ApplicationConfig
             .startServer(
                 Javalin.create(),
